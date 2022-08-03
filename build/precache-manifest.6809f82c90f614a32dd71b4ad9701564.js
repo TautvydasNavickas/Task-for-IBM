c8c9a856a9761cdff0e9ed3d101a0065e6dda7db
TautvydasNavickas <31541631+TautvydasNavickas@users.noreply.github.com>
2022-08-04 01:45:25 +0300

    Add files via upload

---
+++ build/precache-manifest.6809f82c90f614a32dd71b4ad9701564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3a862e23ec772ccfe3bbdb03df4052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "281025a5959de17c35d3",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "2c6b746f52bbf2161f8a",
+    "url": "/static/css/main.02755ae0.chunk.css"
+  },
+  {
+    "revision": "281025a5959de17c35d3",
+    "url": "/static/js/2.1d20a912.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.1d20a912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6b746f52bbf2161f8a",
+    "url": "/static/js/main.9965e8db.chunk.js"
+  },
+  {
+    "revision": "31fe52e0bb68f83722f1",
+    "url": "/static/js/runtime-main.7094e3bb.js"
+  }
+]);
